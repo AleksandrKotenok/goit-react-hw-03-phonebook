8c53f2ed47e6409fa39795d1fd570711e3edafc4
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2021-12-20 17:31:05 +0200

    add LC

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,26 @@
-import { AddForm } from '../AddForm/AddForm';
-import { Filter } from '../Filter/Filter';
-import { ContactList } from '../ContactList/ContactList';
-import React, { Component, Fragment } from 'react';
-import { nanoid } from 'nanoid';
+import { AddForm } from "../AddForm/AddForm";
+import { Filter } from "../Filter/Filter";
+import { ContactList } from "../ContactList/ContactList";
+import React, { Component, Fragment } from "react";
+import { nanoid } from "nanoid";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    contacts: [],
+    filter: "",
   };
-
+  #contacts = "contacts";
+  componentDidMount() {
+    const contactsFromLs = JSON.parse(localStorage.getItem(this.#contacts));
+    if (contactsFromLs) this.setState({ contacts: contactsFromLs });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(this.#contacts, JSON.stringify(this.state.contacts));
+    }
+  }
   submit = ({ name, number }) => {
-    const existingName = this.state.contacts.find(
-      contact => contact.name === name,
-    );
+    const existingName = this.state.contacts.find((contact) => contact.name === name);
     if (existingName) {
       alert(`${name} existing name`);
       return;
@@ -33,16 +35,14 @@ class App extends Component {
     }));
   };
   updateContacts = () => {
-    return this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase()),
-    );
+    return this.state.contacts.filter((contact) => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()));
   };
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== contactId),
     }));
   };
-  filterChange = e => {
+  filterChange = (e) => {
     this.setState({
       filter: e.currentTarget.value,
     });
@@ -55,10 +55,7 @@ class App extends Component {
         <AddForm submit={this.submit} />
         <h2>Contacts</h2>
         <Filter filter={this.state.filter} input={this.filterChange} />
-        <ContactList
-          updatedContacts={this.updateContacts()}
-          deleteContact={this.deleteContact}
-        />
+        <ContactList updatedContacts={this.updateContacts()} deleteContact={this.deleteContact} />
       </Fragment>
     );
   }
